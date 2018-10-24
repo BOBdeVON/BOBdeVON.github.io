91bed6d0f77bc6af745eb7938a2d8f05357dfb2f
BOBdeVON <44219690+BOBdeVON@users.noreply.github.com>
2018-10-24 14:51:21 +1100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ red = [0, 100, 63];
 orange = [40, 100, 60];
 green = [75, 100, 40];
 blue = [196, 77, 55];
-purple = [280, 50, 50];
+purple = [280, 50, 100];
 letterColors = [red, orange, green, blue, purple];
 
 // This variable controls the smallest distance at which a mouse will 
